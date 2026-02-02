8cb646bed7bb8db8fe79a0541361baabfd07d05c
John Nevin <jdn1995@mac.mynetworksettings.com>
2026-02-02 14:38:40 -0500

    Update to github url
    Set base url for internal links to /nevindev.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://nevindev.github.io',
+  base: '/nevindev',
   integrations: [mdx(), sitemap()],
 
   vite: {
